bdacb2d53c98cda86c71229482a14de51c975d7d
avivharuzi <avivharuzi@gmail.com>
2018-06-15 11:26:50 +0300

    added page token route and more fixes

--- server/handlers/youtube.handler.js
+++ server/handlers/youtube.handler.js
@@ -2,22 +2,38 @@ const { google } = require('googleapis');
 const youtube = google.youtube('v3');
 const apiKey = process.env.YOUTUBE_API_KEY;
 
-class YoutubeHandler {
-    static async getVideos(q) {
-        const options = {
-            auth: apiKey,
-            part: 'snippet',
-            q: q,
-            maxResults: 5,
-            type: 'video'
-        }
+const options = {
+    auth: apiKey,
+    part: 'snippet',
+    maxResults: 5,
+    type: 'video'
+}
 
-        try {
-            const result = await youtube.search.list(options);
-            return result.data;
-        } catch (e) {
-            return e;
-        }
+class YoutubeHandler {
+    static getVideosBySearch(q) {
+        return new Promise((resolve, reject) => {
+            const videosOptions = options;
+            videosOptions.q = q;
+
+            youtube.search.list(videosOptions)
+                .then(response => {
+                    resolve(response.data);
+                })
+                .catch(reject);
+        });
+    }
+
+    static async getVideosByPage(pageToken) {
+        return new Promise((resolve, reject) => {
+            const videosOptions = options;
+            videosOptions.pageToken = pageToken;
+
+            youtube.search.list(videosOptions)
+                .then(response => {
+                    resolve(response.data);
+                })
+                .catch(reject);
+        });
     }
 }
 
--- server/routes/youtube.route.js
+++ server/routes/youtube.route.js
@@ -4,12 +4,21 @@ const router = express.Router();
 const RouteHandler = require('./../handlers/route.handler');
 const YoutubeHandler = require('./../handlers/youtube.handler');
 
-router.get('/search/:q', async (req, res) => {
+router.get('/videos/search/:q', async (req, res) => {
     try {
-        const videos = await YoutubeHandler.getVideos(req.params.q);
+        const videos = await YoutubeHandler.getVideosBySearch(req.params.q);
         res.send(videos);
     } catch (e) {
-        RouteHandler.error(res, 409, '', e);
+        RouteHandler.error(res, 409, 'There was problem to find videos');
+    }
+});
+
+router.get('/videos/page/:pageToken', async (req, res) => {
+    try {
+        const videos = await YoutubeHandler.getVideosByPage(req.params.pageToken);
+        res.send(videos);
+    } catch (e) {
+        RouteHandler.error(res, 409, 'There page token is invalid no videos was found');
     }
 });
 
