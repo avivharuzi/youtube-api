30bdf9f439c3ca29e32370fa60cac5cfdfeb9164
avivharuzi <avivharuzi@gmail.com>
2018-06-15 12:07:54 +0300

    added youtube urls

--- client/src/app/constants/urls.ts
+++ client/src/app/constants/urls.ts
@@ -1,4 +1,11 @@
-const BASE_URL: string = 'http://localhost:8080';
+const BASE_URL: string = 'http://localhost:3000';
 
 export const DEFAULT_IMAGE_PATH: string = 'assets/images/defaults/default-image.png';
 export const LOADING_GIF_PATH: string = 'assets/images/tools/loading.gif';
+
+const BASE_API_URL: string = `${BASE_URL}/api`;
+
+const BASE_YOUTUBE_URL: string = `${BASE_API_URL}/youtube`;
+
+export const YOUTUBE_VIDEOS_SEARCH: string = `${BASE_YOUTUBE_URL}/videos/search`;
+export const YOUTUBE_VIDEOS_PAGE_TOKEN: string = `${BASE_YOUTUBE_URL}/videos/page`;
