435e016afe61a15ef7b9161ab1c4942d4b2c119a
avivharuzi <avivharuzi@gmail.com>
2018-06-15 10:53:00 +0300

    added basic youtube handler

---
+++ server/handlers/youtube.handler.js
@@ -0,0 +1,22 @@
+const { google } = require('googleapis');
+const youtube = google.youtube('v3');
+const apiKey = process.env.YOUTUBE_API_KEY;
+
+class YoutubeHandler {
+    static async getVideos(q) {
+        const options = {
+            auth: apiKey,
+            part: 'snippet',
+            q: q,
+            maxResults: 5,
+            type: 'video'
+        }
+
+        try {
+            const result = await youtube.search.list(options);
+            return result.data;
+        } catch (e) {
+            return e;
+        }
+    }
+}
