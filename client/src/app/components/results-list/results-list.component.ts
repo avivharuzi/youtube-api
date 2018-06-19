d824fad50a0170ebdc70fed97c3f07e41f1bef8a
avivharuzi <avivharuzi@gmail.com>
2018-06-19 08:50:33 +0300

    update results-list

--- client/src/app/components/results-list/results-list.component.ts
+++ client/src/app/components/results-list/results-list.component.ts
@@ -8,10 +8,13 @@ import { Router } from '@angular/router';
 })
 export class ResultsListComponent {
   @Input() public results: any;
+  @Input() public isPopular: boolean;
 
   constructor(
     private router: Router
-  ) { }
+  ) {
+    this.isPopular = false;
+  }
 
   watch(v: string): void {
     this.router.navigate(['/watch'], { queryParams: { v: v } });
