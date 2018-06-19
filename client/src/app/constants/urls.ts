2a223c940a80db994b8d7d4d8c4858d2103528ab
avivharuzi <avivharuzi@gmail.com>
2018-06-19 16:45:53 +0300

    update server old port in client

--- client/src/app/constants/urls.ts
+++ client/src/app/constants/urls.ts
@@ -1,4 +1,4 @@
-const BASE_URL: string = 'http://localhost:8080';
+const BASE_URL: string = 'http://localhost:3000';
 
 export const DEFAULT_IMAGE_PATH: string = 'assets/images/defaults/default-image.png';
 export const LOADING_GIF_PATH: string = 'assets/images/tools/loading.gif';
