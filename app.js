5caa73a97fd0002c99d02b9f465b1f6405285f58
avivharuzi <avivharuzi@gmail.com>
2018-06-20 16:31:40 +0300

    update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes');
 
@@ -16,6 +17,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 routes(app);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 module.exports = app;
